926381bcea8c32d2c7c4dbb438ba253da0aa0435
Fah11ad <falqurashi11@gmail.com>
2020-05-10 17:33:13 +0300

    Updates

---
+++ precache-manifest.1037fd8b15e5e30c1695a3a0fecfdbe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f01582d755b11dcec337e9d96aea8ea",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "41989a5605adcf99f4aa",
+    "url": "/monsters-rolodex/static/css/main.dae00eea.chunk.css"
+  },
+  {
+    "revision": "10b6e3317e3496e273e1",
+    "url": "/monsters-rolodex/static/js/2.76fe6a9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.76fe6a9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41989a5605adcf99f4aa",
+    "url": "/monsters-rolodex/static/js/main.70803c74.chunk.js"
+  },
+  {
+    "revision": "01dbdd15b07eef6d5d61",
+    "url": "/monsters-rolodex/static/js/runtime-main.0ac85f65.js"
+  }
+]);
